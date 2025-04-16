a95ce224a6c36f1ddf0dd22c0432ef6c7caaa41a
HuySG <huybc2011@gmail.com>
2025-04-16 15:56:07 +0700

    update new

--- src/app/Projects/[id]/page.tsx
+++ src/app/Projects/[id]/page.tsx
@@ -1,42 +1,39 @@
-"use client";
-import React, { useState } from "react";
-import ProjectHeader from "../ProjectHeader";
-import BoardView from "../BoardView";
-import List from "../ListView";
-import Timeline from "../TimelineView";
-import TableView from "../TableView";
-import ModalNewTask from "@/components/ModalNewTask";
-type Props = {
-  params: { id: string };
-};
+// "use client";
+// import React, { useState } from "react";
+// import ProjectHeader from "../ProjectHeader";
+// import ModalNewTask from "@/components/ModalNewTask";
 
-const Project = ({ params }: Props) => {
-  const { id } = params;
-  const [activeTab, setActiveTab] = useState("Board");
-  const [isModalNewTaskOpen, setIsModalNewTaskOpen] = useState(false);
-  return (
-    <div>
-      {/* MODAL NEW TASKS */}
-      <ModalNewTask
-        isOpen={isModalNewTaskOpen}
-        onClose={() => setIsModalNewTaskOpen(false)}
-        id={id}
-      />
-      <ProjectHeader activeTab={activeTab} setActiveTab={setActiveTab} />
-      {activeTab === "Board" && (
-        <BoardView id={id} setIsModaNewTasklOpen={setIsModalNewTaskOpen} />
-      )}
-      {activeTab === "List" && (
-        <List id={id} setIsModaNewTasklOpen={setIsModalNewTaskOpen} />
-      )}
-      {activeTab === "Timeline" && (
-        <Timeline id={id} setIsModalNewTaskOpen={setIsModalNewTaskOpen} />
-      )}
-      {activeTab === "Table" && (
-        <TableView id={id} setIsModalNewTaskOpen={setIsModalNewTaskOpen} />
-      )}
-    </div>
-  );
-};
+// type Props = {
+//   params: { id: string };
+// };
 
-export default Project;
+// const Project = ({ params }: Props) => {
+//   const { id } = params;
+//   const [activeTab, setActiveTab] = useState("Board");
+//   const [isModalNewTaskOpen, setIsModalNewTaskOpen] = useState(false);
+//   return (
+//     // <div>
+//     //   {/* MODAL NEW TASKS */}
+//     //   <ModalNewTask
+//     //     isOpen={isModalNewTaskOpen}
+//     //     onClose={() => setIsModalNewTaskOpen(false)}
+//     //     id={id}
+//     //   />
+//     //   <ProjectHeader activeTab={activeTab} setActiveTab={setActiveTab} />
+//     //   {activeTab === "Board" && (
+//     //     <BoardView id={id} setIsModaNewTasklOpen={setIsModalNewTaskOpen} />
+//     //   )}
+//     //   {activeTab === "List" && (
+//     //     <List id={id} setIsModaNewTasklOpen={setIsModalNewTaskOpen} />
+//     //   )}
+//     //   {activeTab === "Timeline" && (
+//     //     <Timeline id={id} setIsModalNewTaskOpen={setIsModalNewTaskOpen} />
+//     //   )}
+//     //   {activeTab === "Table" && (
+//     //     <TableView id={id} setIsModalNewTaskOpen={setIsModalNewTaskOpen} />
+//     //   )}
+//     // </div>
+//   );
+// };
+
+// export default Project;
